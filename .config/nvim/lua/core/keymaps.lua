9cda2764f5436db4adefe8029b984207254a6675
Nathan Roark <nathanroark@protonmail.com>
2024-10-02 18:17:58 -0500

    skrskr

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -2,8 +2,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+local map = vim.keymap.set
+
 -- Map <leader>w to :w (save)
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
+map('n', '<leader>w', ':w<CR>', {    noremap = true, silent = true })
 
 -- Map <leader>q to :q (quit)
-vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+map('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+
+-- nvimtree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 24.0
+config.font_size = 21.0
 -- config.color_scheme = "rose-pine"
 -- config.color_scheme = "Catppuccin Mocha"
 config.color_scheme = "Tokyo Night"
