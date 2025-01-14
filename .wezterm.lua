ae11f0c996c3e8daed8b3ce10be75de435e9ace6
Nathan Roark <nathanroark@protonmail.com>
2025-01-13 18:04:35 -0600

    test

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono")
-config.font_size = 18.0
+config.font_size = 20.0
 -- config.color_scheme = "rose-pine"
 -- config.color_scheme = "Catppuccin Mocha"
 config.color_scheme = "Rose Pine"
