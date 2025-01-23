7f8cc2ef11d042638e90641e117ac8962f92914d
Nathan Roark <nathanroark@protonmail.com>
2025-01-22 18:37:04 -0600

    nontransparent

--- .config/nvim/lua/plugins/astroui.lua
+++ .config/nvim/lua/plugins/astroui.lua
@@ -10,14 +10,14 @@ return {
     -- colorscheme = "rose-pine-main",
     colorscheme = "tokyonight-night",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        Normal = { bg = "#000000" },
-      },
-    },
+    -- highlights = {
+    --   init = { -- this table overrides highlights in all themes
+    --     Normal = { bg = "#000000" },
+    --   },
+    --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+    --     Normal = { bg = "#000000" },
+    --   },
+    -- },
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
--- .wezterm.lua
+++ .wezterm.lua
@@ -6,16 +6,16 @@ local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 20.0
--- config.color_scheme = "rose-pine"
+config.color_scheme = "rose-pine"
 -- config.color_scheme = "Catppuccin Mocha"
-config.color_scheme = "Rose Pine"
-config.colors = {
-	-- The default text color
-	-- foreground = "silver",
-	-- The default background color
-	-- background = "hsl(240, 15%, 13%)",
-	background = "hsl(240, 0%, 0%)",
-}
+-- config.color_scheme = "Rose Pine"
+-- config.colors = {
+-- The default text color
+-- foreground = "silver",
+-- The default background color
+-- background = "hsl(240, 15%, 13%)",
+-- background = "hsl(240, 0%, 0%)",
+-- }
 
 config.window_padding = {
 	left = 0,
@@ -37,8 +37,8 @@ config.window_padding = {
 
 --Requires Wayland
 -- config.enable_wayland = true
-config.window_background_opacity = 0.70
-config.macos_window_background_blur = 0
+-- config.window_background_opacity = 0.70
+-- config.macos_window_background_blur = 0
 -- config.window_background_opacity = 1.0
 
 -- Use ALT instead of SHIFT to bypass application mouse reporting
